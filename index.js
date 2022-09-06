bc873418ca243e1328b01e3878e25cb50df2c8e7
Ian LS <iansouza.sjc@gmail.com>
2022-09-06 08:56:04 -0300

    refactor resolvers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,37 +9,7 @@ app.get('/', (req, res) => {
   res.send('GraphQL is amazing');
 });
 
-class Product {
-  constructor(id, { name, desc, price, soldOut, stores }) {
-    this.id = id;
-    this.name = name;
-    this.desc = desc;
-    this.price = price;
-    this.soldOut = soldOut;
-    this.stores = stores;
-  }
-}
-
-const productDatabase = {};
-
-const root = {
-  product: () => {
-    return {
-      id: 21384,
-      name: 'widget',
-      desc: 'Beautiful widget to use in your garden',
-      price: 34.99,
-      soldOut: false,
-      stores: [{ store: 'Passadena' }, { store: 'Los Angeles' }],
-    };
-  },
-
-  createProduct: ({ input }) => {
-    let id = require('crypto').randomBytes(10).toString('hex');
-    productDatabase[id] = input;
-    return new Product(id, input);
-  },
-};
+const root = resolvers;
 
 app.use(
   '/graphql',
