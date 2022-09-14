807356c6af533001c6770f09c2e3a4bcbd43fdf8
Ian LS <iansouza.sjc@gmail.com>
2022-09-14 08:17:14 -0300

    create sequelize connection

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -1,4 +1,7 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
+import { sequelize, DataTypes } from 'sequelize';
+import _ from 'lodash';
+import casual from 'casual';
 
 // Mongo connection
 mongoose.Promise = global.Promise;
@@ -29,4 +32,23 @@ const widgetSchema = new mongoose.Schema({
 
 const Widgets = mongoose.model('widgets', widgetSchema);
 
-export { Widgets };
+// Sequelize connection
+
+// creates a db sqlite inside of the memory of local system
+const sequelize = new Sequelize('sqlite::memory');
+
+const Categories = sequelize.define('categories', {
+  category: DataTypes.STRING,
+  desc: DataTypes.STRING,
+});
+
+Categories.sync({ force: true }).then(() => {
+  _.times(5, (i) => {
+    Categories.create({
+      category: casual.word,
+      desc: casual.sentence,
+    });
+  });
+});
+
+export { Widgets, Categories };
